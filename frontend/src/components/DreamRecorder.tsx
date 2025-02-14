daa684c0794eda15579979af18657aa47e94d05f
sairammr <sairam1203mr@gmail.com>
2025-02-14 13:32:32 +0530

    NFT creation modal fix

--- frontend/src/components/DreamRecorder.tsx
+++ frontend/src/components/DreamRecorder.tsx
@@ -230,6 +230,7 @@ function DreamRecorder() {
         const tx = await contract.safeMint(userAddress, metadataCID);
 
         const receipt = await tx.wait();
+        setShowModal(false);
         return {
             success: true,
             metadataURI: `ipfs://${metadataCID}`,
@@ -270,7 +271,7 @@ function DreamRecorder() {
                   className="absolute -bottom-16 left-1/2 transform -translate-x-1/2 whitespace-nowrap"
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
-                  transition={{ delay: 0.3 }}
+                  transition={{ delay: 0.01 }}
                 >
                   <Wand2 className="inline-block mr-2 h-5 w-5 text-white" />
                   <span className="text-white uppercase">Processing Dream...</span>
@@ -304,8 +305,8 @@ function DreamRecorder() {
                 >
                   <div className="relative p-12">
                     {isRecording ? (
-                      <div className="relative" style={{ scale: 3 }}>
-                        <Orb
+                      <div className="relative w-500" style={{  }}>
+                        <Orb 
                           hoverIntensity={1.11}
                           rotateOnHover={true}
                           hue={288}
@@ -352,32 +353,7 @@ function DreamRecorder() {
             </motion.div>
           )}
 
-          <AnimatePresence>
-            {showControls && (
-              <motion.div
-                className="w-full space-y-6"
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -20 }}
-              >
-                <motion.button
-                  className="w-full py-6 bg-black text-white border-2 border-white hover:bg-white hover:text-black transition-all font-bold uppercase"
-                  whileHover={{ scale: 1.02, x: 5 }}
-                  whileTap={{ scale: 0.98 }}
-                  onClick={createNFT}
-                >
-                  Mint Dream as NFT
-                </motion.button>
-                <motion.button
-                  className="w-full py-6 bg-white text-black border-2 border-white hover:bg-black hover:text-white transition-all font-bold uppercase"
-                  whileHover={{ scale: 1.02, x: -5 }}
-                  whileTap={{ scale: 0.98 }}
-                >
-                  Share on Twitter
-                </motion.button>
-              </motion.div>
-            )}
-          </AnimatePresence>
+         
         </div>
       </div>
       <AnimatePresence>
